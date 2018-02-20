08c6d9edfbd2b0b9ebb2b8e5f2a5dd89c7bcefdb
balakinsergey83 <balakinsergey83@gmail.com>
2018-02-20 02:00:52 +0200

    DZ-4.7

--- DZ-4.7/Program.cs
+++ DZ-4.7/Program.cs
@@ -10,6 +10,55 @@ namespace DZ_4._7
     {
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine();
+            Console.WriteLine("    1 - е    Авто");
+            Console.WriteLine();
+            Console.Write("     Название авто               ");
+            string a = Console.ReadLine();
+            Console.Write("     Максимальна скорость        ");
+            int a1 = int.Parse(Console.ReadLine());
+            Console.Write("     Расход литров на 100 км     ");
+            int a2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("    2 - е    Авто");
+            Console.WriteLine();
+            Console.Write("     Название авто               ");
+            string b = Console.ReadLine();
+            Console.Write("     Максимальна скорость        ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.Write("     Расход литров на 100 км     ");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("    3 - е    Авто");
+            Console.WriteLine();
+            Console.Write("     Название авто               ");
+            string c = Console.ReadLine();
+            Console.Write("     Максимальна скорость        ");
+            int c1 = int.Parse(Console.ReadLine());
+            Console.Write("     Расход литров на 100 км     ");
+            int c2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("     Сравнение по максимальной скорости");
+            Console.WriteLine();
+            Console.WriteLine($"     {a,10}     {b,10}     {c,10}");
+            Console.WriteLine($"     {a1,10}    {b1,10}    {c1,10}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("     Сравнение по расходу литров на 100 км");
+            Console.WriteLine();
+            Console.WriteLine($"     {a,10}     {b,10}     {c,10}");
+            Console.WriteLine($"     {a2,10}    {b2,10}    {c2,10}");
+
+            Console.ReadKey();
+
+
         }
     }
 }
